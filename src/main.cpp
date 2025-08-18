e476227682a254b5f490dea5248928da86421db0
Can <pekkan.ct@gmail.com>
2025-08-18 08:59:54 -0400

    added config
    capitalize

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "Game.h"
+#include "card.h"
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  Game game{};
+  game.m_suitPiles[0].add(Card{Card::Suit::club, Card::Face::ace});
+  game.m_suitPiles[1].add(Card{Card::Suit::heart, Card::Face::ace});
+  game.m_suitPiles[2].add(Card{Card::Suit::diamond, Card::Face::ace});
+  game.m_suitPiles[3].add(Card{Card::Suit::spade, Card::Face::ace});
+  for (int i{0}; i < 54; i++) {
+    std::cout << game << std::endl;
+    game.m_deck.draw();
+  }
+  return 0;
+}
