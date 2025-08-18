025056afcfdd7b7252a4cc0b4178bc66db04b274
Can <pekkan.ct@gmail.com>
2025-08-18 08:59:11 -0400

    created decks

---
+++ src/deck.cpp
@@ -0,0 +1,53 @@
+#include "deck.h"
+#include "card.h"
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <random>
+
+Deck::Deck() {
+  for (int i{0}; i <= static_cast<int>(Card::Suit::diamond); i++) {
+    Card::Suit suit{static_cast<Card::Suit>(i)};
+    for (int j{1}; j <= static_cast<int>(Card::Face::king); j++) {
+      Card::Face face{static_cast<Card::Face>(j)};
+      m_deck.push_back(Card{suit, face});
+    }
+  }
+
+  shufle();
+
+  assert(m_deck.size() == 52 && "Is the deck a full deck");
+}
+
+void Deck::shufle() {
+  std::mt19937 s_generator(std::random_device{}());
+
+  std::shuffle(m_deck.begin(), m_deck.end(), s_generator);
+}
+
+void Deck::draw() {
+  if (m_deck.empty()) {
+    while (!m_deckPile.empty()) {
+      m_deck.push_back(m_deckPile.back());
+      m_deckPile.pop_back();
+    }
+  } else {
+    m_deckPile.push_back(m_deck.back());
+    m_deck.pop_back();
+  }
+}
+
+std::ostream &operator<<(std::ostream &cout, const Deck &deck) {
+  if (deck.m_deck.empty()) {
+    cout << "   ";
+  } else {
+    cout << "\u2588\u2588 "; // █ full block
+  }
+  if (deck.m_deckPile.empty()) {
+    cout << "   ";
+  } else {
+    cout << deck.m_deckPile.back();
+  }
+  return cout;
+}
---
+++ src/deck.h
@@ -0,0 +1,24 @@
+#ifndef CAN_DECK_H
+#define CAN_DECK_H
+
+#include "card.h"
+#include <list>
+#include <ostream>
+#include <vector>
+
+class Deck {
+private:
+  std::vector<Card> m_deck{};
+  std::vector<Card> m_deckPile{};
+
+public:
+  Deck();
+
+  void draw();
+
+  void shufle();
+
+  friend std::ostream &operator<<(std::ostream &cout, const Deck &deck);
+};
+
+#endif // !CAN_DECK_H
